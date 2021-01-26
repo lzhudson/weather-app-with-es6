c7c7f59ceb3baefd4906f56c1ffaedec84f61455
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-26 10:07:22 -0300

    refactor: add unit metric in request weather data

--- src/weather.js
+++ src/weather.js
@@ -6,7 +6,7 @@ export default class Weather {
   }
 
   async getWeather() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}&units=metric`);
     const responseData = await response.json();
 
     return responseData;
