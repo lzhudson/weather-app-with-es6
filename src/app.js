7bd2a15cfc6b38fd00dc5af5ab685f0bdbf105c1
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-27 19:18:56 -0300

    feat: set in local storage city and country after change location

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ document.getElementById('w-change-btn').addEventListener('click', (e) => {
   const city = document.getElementById('city').value;
   const country = document.getElementById('country').value;
   weather.changeLocation(city, country);
+  storage.setLocationData(city, country);
   getWeather();
   $('#locModal').modal('hide');
 });
