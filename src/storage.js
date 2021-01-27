45a061ded1926dde955449e65cbf50fa12d9b2c6
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-27 19:12:52 -0300

    feat: create method to get in localStorage city and country

--- src/storage.js
+++ src/storage.js
@@ -6,6 +6,24 @@ class Storage {
     this.defaultCountry = 'BR';
   }
 
+  getLocationData() {
+    if(localStorage.getItem('city') === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem('city');
+    }
+    if(localStorage.getItem('country') === null) {
+      this.country = this.defaultCity;
+    } else {
+      this.country = localStorage.getItem('country');
+    }
+
+    return {
+      city: this.city,
+      country: this.country
+    }
+  }
+
   setLocationData(city, country) {
     localStorage.setItem('city', city);
     localStorage.setItem('state', country);
