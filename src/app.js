be0610487874827d320b7efba0df9012eab32b3a
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-27 19:06:46 -0300

    feat: add event to change location and show new weather data

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ const ui = new UI();
 
 weather.changeLocation('São Paulo', 'BR');
 
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+  weather.changeLocation(city, country);
+  getWeather();
+  $('#locModal').modal('hide');
+});
+
 function getWeather() {
   weather.getWeather()
     .then(results => {
